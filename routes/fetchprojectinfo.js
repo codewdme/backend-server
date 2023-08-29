df52d8c5b6888a7a00a3c28209aea4c509ea4c47
codewdme <lll.rg3.lll@gmail.com>
2023-08-29 08:56:53 +0530

    corecting module locations

--- routes/fetchprojectinfo.js
+++ routes/fetchprojectinfo.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const projectInfo = require("../../models/projectInfo");
+const projectInfo = require("../models/projectInfo");
 
 // using get request to fetch file info from mongo db by passing query parameters in params.
 router.get(
