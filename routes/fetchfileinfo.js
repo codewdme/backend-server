56d3dd9e156772e7123f2d37cad39fac22c81dfd
codewdme <lll.rg3.lll@gmail.com>
2023-09-08 11:47:03 +0530

    adding category name to fech files

--- routes/fetchfileinfo.js
+++ routes/fetchfileinfo.js
@@ -3,28 +3,30 @@ const router = express.Router();
 const fileInfo = require("../models/fileInfo");
 
 // using get request to fetch file info from mongo db by passing query parameters in params.
-router.get(
-  "/fetchfileinfo/:course/:semester",
+router.get("/fetchfileinfo/:category/:course/:semester", async (req, res) => {
+  // converting params value to the value that database can read
+  let recievedCourseName = req.params["course"];
+  let courseName = recievedCourseName.replace(/\./g, "").toUpperCase();
 
-  async (req, res) => {
-    // converting params value to the value that database can read
-    let originalString = req.params["course"];
-    let courseName = originalString.replace(/\./g, "").toUpperCase();
+  // setting value of sem from params
+  let sem = req.params["semester"];
+  let recievedCategoryName = req.params["category"];
+  let categoryName = recievedCategoryName.replace(/\./g, "").toUpperCase();
 
-    // setting value of sem from params
-    let sem = req.params["semester"];
+  // query for mongo db
 
-    // query for mongo db
+  let filesData = await fileInfo.find({
+    category: categoryName,
+    semester: sem,
+    course: courseName,
+  });
 
-    let filesData = await fileInfo.find({ semester: sem, course: courseName });
-
-    try {
-      res.send(filesData);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("some error Occured");
-    }
+  try {
+    res.send(filesData);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some error Occured");
   }
-);
+});
 
 module.exports = router;
