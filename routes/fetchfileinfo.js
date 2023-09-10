79bb339a6ccc49833edfa2d056ee1f616c9b4f16
codewdme <lll.rg3.lll@gmail.com>
2023-09-10 10:22:35 +0530

    sorting added to fetch fileinfo

--- routes/fetchfileinfo.js
+++ routes/fetchfileinfo.js
@@ -12,14 +12,17 @@ router.get("/fetchfileinfo/:category/:course/:semester", async (req, res) => {
   let sem = req.params["semester"];
   let recievedCategoryName = req.params["category"];
   let categoryName = recievedCategoryName.replace(/\./g, "").toUpperCase();
-
+  // descending order sort
+  const sort = { year: -1 };
   // query for mongo db
 
-  let filesData = await fileInfo.find({
-    category: categoryName,
-    semester: sem,
-    course: courseName,
-  });
+  let filesData = await fileInfo
+    .find({
+      category: categoryName,
+      semester: sem,
+      course: courseName,
+    })
+    .sort(sort);
 
   try {
     res.send(filesData);
