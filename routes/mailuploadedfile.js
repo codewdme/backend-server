6939fbce7c13ae58717592cdb0bdf08f3981e3ad
codewdme <lll.rg3.lll@gmail.com>
2023-09-15 16:54:55 +0530

    editing the s3 storage system

--- routes/mailuploadedfile.js
+++ routes/mailuploadedfile.js
@@ -6,17 +6,21 @@ const path = require("path");
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: "../uploads/",
-  filename: (req, file, callback) => {
-    callback(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
+const AWS = require("aws-sdk");
+
+// Configure AWS SDK with your credentials
+AWS.config.update({
+  accessKeyId: "ASIAT3QG5RJK5CQQ2GY4",
+  secretAccessKey: "5ZLuty8kP2Czrhu6vUxtlBUeqRjREJeAmvsgZIsY",
+  region: "eu-central-1",
 });
 
-const upload = multer({ storage });
+// Create an S3 instance
+const s3 = new AWS.S3();
+
+const upload = multer();
+
+const bucketName = "cyclic-naughty-lion-train-eu-central-1";
 
 const transporter = nodemailer.createTransport({
   service: "Gmail", // Use your email service
@@ -26,34 +30,50 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-router.post("/mailuploadedfiles", upload.single("file"), (req, res) => {
+router.post("/upload", upload.single("file"), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded." });
   }
 
-  const mailOptions = {
-    from: "junexus.edgerunners@gmail.com",
-    to: "creator.techwhiz@gmail.com", // Replace with the recipient's email
-    subject: "File Attachment",
-    text: "Please find the attached file.",
-    attachments: [
-      {
-        filename: req.file.filename,
-        path: req.file.path,
-      },
-    ],
+  // Define the file key and upload parameters
+  const fileKey = `uploads/${Date.now()}-${req.file.originalname}`;
+  const params = {
+    Bucket: bucketName,
+    Key: fileKey,
+    Body: req.file.buffer, // Use req.file.buffer to access the uploaded file data
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Email error:", error);
-      return res
-        .status(500)
-        .json({ message: "File upload and email sending failed." });
-    }
-    console.log("Email sent:", info.response);
-    res.json({ message: "File uploaded and email sent successfully." });
-  });
+  try {
+    await s3.upload(params).promise();
+    res.json({ message: "File uploaded to S3 successfully." });
+  } catch (error) {
+    console.error("S3 upload error:", error);
+    res.status(500).json({ message: "Error uploading file to S3." });
+  }
+});
+
+const mailOptions = {
+  from: "junexus.edgerunners@gmail.com",
+  to: "creator.techwhiz@gmail.com", // Replace with the recipient's email
+  subject: "File Attachment",
+  text: "Please find the attached file.",
+  attachments: [
+    {
+      filename: req.file.filename,
+      path: req.file.path,
+    },
+  ],
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error("Email error:", error);
+    return res
+      .status(500)
+      .json({ message: "File upload and email sending failed." });
+  }
+  console.log("Email sent:", info.response);
+  res.json({ message: "File uploaded and email sent successfully." });
 });
 
 module.exports = router;
