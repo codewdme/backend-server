0cb3d8988e51d2ef1eb153d2e18fd68643ffa0b8
codewdme <lll.rg3.lll@gmail.com>
2023-09-18 19:09:35 +0530

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 connectToMongo();
 const app = express();
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.use(express.json());
 app.use(cors());
